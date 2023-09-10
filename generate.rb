146834692ca957394dd4c17fe7e7b607b65959d7
Troels Busk-Jepsen <troelsbusk@gmail.com>
2023-09-10 13:39:55 +0200

    Clear foldes before processing

--- generate.rb
+++ generate.rb
@@ -1,5 +1,22 @@
 require 'erb'
 
+require 'fileutils'
+
+# Define an array of relative paths to the folders you want to clear
+relative_paths = ['lilypond', 'pdf']
+
+# Iterate over each relative path and clear the corresponding folder
+relative_paths.each do |relative_path|
+  # Construct the absolute path for the current folder
+  absolute_path = File.expand_path(relative_path, __dir__)
+
+  # Clear the folder (delete all files and subdirectories)
+  FileUtils.rm_rf(absolute_path)
+
+  # Optionally, you can recreate the folder if needed
+  FileUtils.mkdir_p(absolute_path)
+end
+
 # Sample data structure (you can replace this with your actual data)
 music_pieces = [
     # { notes: 'b 8 dis fis 4 g 8 e c 4', descending: "g 8 e c 4 b 8 dis fis 4"},
@@ -28,40 +45,5 @@ music_pieces.each_with_index do |piece, index|
   File.open(filename, 'w') { |file| file.write(lilypond_code) }
   puts "Generated #{filename}"
 end
-
-
-
-
-
-
-
-
-# require 'erb'
-
-# # Load the ERB template
-# template = ERB.new(File.read('template.erb'))
-
-# patterns = [
-#     'b 8 dis fis 4 g 8 e c 4',
-#     'b 8 dis fis 4 as 8 f des 4',
-#     'b 8 dis fis 4 g 8 c e 4',
-#     "c' 8 d e g a 2 b 8 a fis e d 2",
-#     "b 8 dis fis 4 f 8 a c 4 d 8 b g 4 as 8 f des 4",
-#     "c' 8 es g bes a g f e",
-#     "c' 16 d es e f e es d c 2",
-#     "d' 8 es f e es 2"
-# ]
-
-# patterns.each_with_index do |pattern, index|
-
-#     # Render the ERB template with the variables
-#     result = template.result(binding)
-
-    
-#     File.open("lilypond/output_filename-#{index}.ly", 'w') do |file|
-#         file.write(result)
-#       end
-
-# end
 
 
