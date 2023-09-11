0e4240b4614eb513791845c7a55b2ec540d0028c
Troels Busk-Jepsen <troelsbusk@gmail.com>
2023-09-11 22:07:54 +0200

    Concatenate pdf's

--- build.sh
+++ build.sh
@@ -14,3 +14,5 @@ mkdir -p "$destination_directory"
 find "$source_directory" -name "*.ly" -print0 | xargs -0 -I {} lilypond -o "$destination_directory" "{}"
 
 echo "All LilyPond files processed and placed in $destination_directory."
+
+pdftk pdf/*.pdf cat output pdf/8tonic_line_compendium.pdf
